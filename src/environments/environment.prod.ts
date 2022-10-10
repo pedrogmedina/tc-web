081c4651c1f22c802c29c71661b1960e3c0c0536
pedrogmedina <perdy_184@outlook.es>
2022-10-10 19:16:51 +0200

    feat(develop): environment for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNSLavXg9NXFl-ZMBwB4YP1TAlpYsudUI",
+    authDomain: "letourcaravan-database.firebaseapp.com",
+    databaseURL: "https://letourcaravan-database-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "letourcaravan-database",
+    storageBucket: "letourcaravan-database.appspot.com",
+    messagingSenderId: "843236154751",
+    appId: "1:843236154751:web:5ef58184c3f0995be3ce4a",
+    measurementId: "G-V8Z6CFHH7V"
+  }
 };
