6920cf9a8acc253ef52e92b30b4d4f394f1d73d7
pedrogmedina <perdy_184@outlook.es>
2022-10-23 12:53:31 +0200

    feat(develop): comment firestore

--- src/services/tours.service.ts
+++ src/services/tours.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Tour } from 'src/models/tour';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { AngularFireList } from '@angular/fire/compat/database';
+// import { Firestore } from '@angular/fire/firestore';
+// import { where, query, collection } from '@angular/fire/firestore';
+// import { Observable } from '@firebase/util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,8 @@ export class ToursService {
   myToursRef : AngularFireList<Tour>;
 
   constructor(
-    private db: AngularFireDatabase
+    private db: AngularFireDatabase,
+    // private afs: Firestore
   ) { 
     this.myToursRef = db.list(this.dbTours);
   }
@@ -21,6 +25,8 @@ export class ToursService {
     return this.myToursRef;
   }
 
-  getTourFilter( filtro: string ) {
-  }
+  // getTourFilter( filtro: string ) {
+  //   const q = query(collection(this.afs, 'infoRoutes'), where('country', '==', filtro));
+  //   return q;
+  // }
 }
